551020a75176a174679362778db3de54f1fbbac8
Beabocco <163515652+Beabocco@users.noreply.github.com>
2024-04-20 11:50:09 +0200

    Update FileOutputStrategy.java

--- src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.concurrent.ConcurrentHashMap;
+
 //the class name should match the name of the file
 public class FileOutputStrategy implements OutputStrategy {
 
