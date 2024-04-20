402002ba4d3aa07d80303962337fb42aa37aeaab
Beabocco <163515652+Beabocco@users.noreply.github.com>
2024-04-20 18:30:19 +0200

    Update BloodSaturationDataGenerator.java

--- src/main/java/com/cardio_generator/generators/BloodSaturationDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/BloodSaturationDataGenerator.java
@@ -3,11 +3,19 @@ package com.cardio_generator.generators;
 import java.util.Random;
 
 import com.cardio_generator.outputs.OutputStrategy;
-
+/**
+ * Generates simulated blood saturation data for patients.
+ * This class simulates blood saturation levels for each patient and outputs the data using the provided output strategy.
+ */
 public class BloodSaturationDataGenerator implements PatientDataGenerator {
     private static final Random random = new Random();
     private int[] lastSaturationValues;
-
+/**
+ * Constructor of BloodSaturationDataGenerator class.
+ * Initializes the last saturation values for each patient with baseline values.
+ *
+ * @param {int} patientCount The number of patients for which data will be generated.
+ */
     public BloodSaturationDataGenerator(int patientCount) {
         lastSaturationValues = new int[patientCount + 1];
 
@@ -16,7 +24,13 @@ public class BloodSaturationDataGenerator implements PatientDataGenerator {
             lastSaturationValues[i] = 95 + random.nextInt(6); // Initializes with a value between 95 and 100
         }
     }
-
+/**
+* Generates blood saturation data for a specific patient and outputs it using the provided strategy.
+*
+* @param {int} patientId The ID of the patient for which data is generated.
+* @param {OutputStrategy} outputStrategy The output strategy used to output the generated data.
+* @throws Exception if an error occurs during data generation or output. It also helps you to identify where the error is.
+*/
     @Override
     public void generate(int patientId, OutputStrategy outputStrategy) {
         try {
