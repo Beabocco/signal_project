3f110d92b7fe4d1eaa15279ee9a9b04aa3bf68c0
Beabocco <163515652+Beabocco@users.noreply.github.com>
2024-04-20 18:00:01 +0200

    Update PatientDataGenerator.java

--- src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
@@ -2,16 +2,13 @@ package com.cardio_generator.generators;
 
 import com.cardio_generator.outputs.OutputStrategy;
 /** 
-*This interface specifies a method for generating simulated patient data, including vital signs,
-* for health monitoring simulations.
-* @param patientId The ID of the patient for whom data is generated.
-* @param outputStrategy The output strategy used to output the generated patient data.
-**/
+*This interface specifies a method for generating simulated patient data for health monitoring simulations.
+*/
 public interface PatientDataGenerator {
 /**
  * Generates simulated patient data for the specified patient ID using the provided output strategy.
- * @param patientId The ID of the patient for whom data is generated.
- * @param outputStrategy The output strategy used to output the generated patient data.
+ * @param {int} patientId The ID of the patient for whom data is generated.
+ * @param {OutputStrategy} outputStrategy The output strategy used to output the generated patient data.
  */
     void generate(int patientId, OutputStrategy outputStrategy);
 }
