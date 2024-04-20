656900f8aeba0cc329024cbae8ba82afb49ef78b
Beabocco <163515652+Beabocco@users.noreply.github.com>
2024-04-20 17:56:13 +0200

    Update FileOutputStrategy.java

--- src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
@@ -6,6 +6,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.concurrent.ConcurrentHashMap;
+/**
+ * Represents an output strategy for storing patient data in files.
+ */
 
 //the class name should match the name of the file
 public class FileOutputStrategy implements OutputStrategy {
@@ -13,12 +16,24 @@ public class FileOutputStrategy implements OutputStrategy {
     private String baseDirectory; //changed variable name to camelCase
 
     public final ConcurrentHashMap<String, String> file_map = new ConcurrentHashMap<>();
-//the constructor should have the same name as the class
+/**
+* Constructs a FileOutputStrategy.
+* @param {String} baseDirectory The base directory where output files will be stored.
+*/
+    //the constructor should have the same name as the class
     public fileOutputStrategy(String baseDirectory) {
 
         this.baseDirectory = baseDirectory; //changed variable name to camelCase
     }
-
+/**
+ * Outputs patient data to files based on the provided patient ID, timestamp, label, and data.
+ *
+ * @param {int} patientId The ID of the patient associated with the data.
+ * @param {long} timeStamp The timestamp indicating when the data was generated.
+ * @param {String} label The label or type of data being output.
+ * @param {String} data The actual data to be written to the file.
+ * @throws IOException if an I/O error occurs while writing to the file.
+ */
     @Override
     public void output(int patientId, long timeStamp, String label, String data) { //canged variable name to camelCase(timeStamp)
         
