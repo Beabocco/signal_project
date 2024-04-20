1ee06c8c59474a899d27721ca25c9de607ec7fcd
Beabocco <163515652+Beabocco@users.noreply.github.com>
2024-04-20 18:00:48 +0200

    Update OutputStrategy.java

--- src/main/java/com/cardio_generator/outputs/OutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/OutputStrategy.java
@@ -1,14 +1,14 @@
 package com.cardio_generator.outputs;
 /**
 *  Defines an interface for output strategies.
-**/
+*/
 public interface OutputStrategy {
 /**
  * Outputs patient data according to the implemented strategy.
- * @param patientId The ID of the patient whose data is being output.
- * @param timestamp The timestamp indicating when the data was generated.
- * @param label The label or type of data being output.
- * @param data The actual data associated with the label.
- **/
+ * @param {int} patientId The ID of the patient whose data is being output.
+ * @param {long} timestamp The timestamp indicating when the data was generated.
+ * @param {String} label The label or type of data being output.
+ * @param {String} data The actual data associated with the label.
+ */
     void output(int patientId, long timestamp, String label, String data);
 }
