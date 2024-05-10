cd00540a5b455ebef32a05d2e779d32d3ce3c880
Beabocco <163515652+Beabocco@users.noreply.github.com>
2024-05-10 11:40:14 +0200

    Update DataReader.java

--- src/main/java/com/data_management/DataReader.java
+++ src/main/java/com/data_management/DataReader.java
@@ -9,5 +9,26 @@ public interface DataReader {
      * @param dataStorage the storage where data will be stored
      * @throws IOException if there is an error reading the data
      */
-    void readData(DataStorage dataStorage) throws IOException;
+    void readData(DataStorage dataStorage) throws IOException{
+         File file = new File(filePath);
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(",");
+                if (parts.length >= 4) {
+                    try {
+                        int patientId = Integer.parseInt(parts[0]);
+                        double measurementValue = Double.parseDouble(parts[1]);
+                        String recordType = parts[2];
+                        long timestamp = Long.parseLong(parts[3]);
+                        dataStorage.addPatientData(patientId, measurementValue, recordType, timestamp);
+                    } catch (NumberFormatException e) {
+                        System.err.println("Error parsing data in line: " + line);
+                    }
+                } else {
+                    System.err.println("Invalid data format in line: " + line);
+                }
+            }
+        }
+    }
 }
